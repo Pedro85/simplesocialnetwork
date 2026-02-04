2f104401d7be81929bfc09fca58da22919346753
Pedro Teixeira <eipedroteixeira@gmail.com>
2026-02-04 21:50:55 +0000

    - Maven configuration - Added basic JUnit tests

---
+++ src/main/java/com/clarityai/simplesocialnetwork/service/SocialService.java
@@ -0,0 +1,4 @@
+package com.clarityai.simplesocialnetwork.service;
+
+public class SocialService {
+}
---
+++ src/test/java/com/clarityai/simplesocialnetwork/SimpleSocialNetworkTest.java
@@ -0,0 +1,60 @@
+package com.clarityai.simplesocialnetwork;
+
+import com.clarityai.simplesocialnetwork.service.SocialService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SimpleSocialNetworkTest {
+
+    private SocialService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new SocialService();
+    }
+
+    @Test
+    void postMessage() {
+
+    }
+
+    @Test
+    void postMessageFail() {
+
+    }
+
+    @Test
+    void postMessages() {
+
+    }
+
+    @Test
+    void readMessages() {
+
+    }
+
+    @Test
+    void readMessagesFail() {
+
+    }
+
+    @Test
+    void followUser() {
+
+    }
+
+    @Test
+    void followUserFail() {
+
+    }
+
+    @Test
+    void viewWall() {
+
+    }
+
+    @Test
+    void viewWallFail() {
+
+    }
+}
